237fbc55edc7e845f020d849f192e526fced71a1
PRAKASHGOWDA RUDRAGOUDA HOSAMANI <124488941+prakashgowda18@users.noreply.github.com>
2024-08-25 18:26:49 +0000

    Update

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ function buttonAnimation(currentKey){
 var activeButton=document.querySelector("."+currentKey).classList.add("pressed");
 setTimeout(function(){
     activeButton.classList.remove("pressed");
-},100);
-// activeButton.classList.remove("pressed");
+},200);
 
 }
